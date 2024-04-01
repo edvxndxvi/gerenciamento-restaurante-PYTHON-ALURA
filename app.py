95a236b7d060a26d653754a22f2b489af1875b38
edvxndxvi <edvandavi1@hotmail.com>
2024-04-01 16:26:33 -0300

    Criando funções para organização

--- app.py
+++ app.py
@@ -1,28 +1,37 @@
 import os #Importando biblioteca Python
 
-print("""
+def exibir_nome_programa():
+    print("""
 Sabor Express
     """)
-
-print("1. Cadastrar Restaurante")
-print("2. Listar Restaurante")
-print("3. Ativar Restaurante")
-print("4. Sair\n")
-
-opcao_escolhida = int(input("Escolha uma opção: "))
-
+def exibir_opcoes():
+    print("1. Cadastrar Restaurante")
+    print("2. Listar Restaurante")
+    print("3. Ativar Restaurante")
+    print("4. Sair\n")
 def finalizar_app():
-    os.system("cls")
+    os.system("cls") # Limpando o CMD
     print("Finalizando programa...")
+def escolher_opcao():
+    opcao_escolhida = int(input("Escolha uma opção: "))
 
-match opcao_escolhida:
-    case 1:
-        print("Cadastrar Restaurante")
-    case 2:
-        print("Listar  Restaurante")
-    case 3:
-        print("Ativar Restaurante")
-    case 4:
-        finalizar_app()
-    case _:
-        print("Opção inválida!")
+    match opcao_escolhida:
+        case 1:
+            print("Cadastrar Restaurante")
+        case 2:
+            print("Listar  Restaurante")
+        case 3:
+            print("Ativar Restaurante")
+        case 4:
+            finalizar_app()
+        case _:
+            print("Opção inválida!")
+
+# Definindo este arquivo como o principal do programa
+def main():
+    exibir_nome_programa()
+    exibir_opcoes()
+    escolher_opcao()
+
+if __name__ ==  '__main__':
+    main()
