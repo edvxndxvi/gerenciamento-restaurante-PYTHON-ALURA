51b70c0f26e99053ef07c15352b2a345f964ec2b
edvxndxvi <edvandavi1@hotmail.com>
2024-04-01 17:16:57 -0300

    Ajustando valores inválidos

--- app.py
+++ app.py
@@ -2,33 +2,46 @@ import os #Importando biblioteca Python
 
 def exibir_nome_programa():
     print("""
-Sabor Express
+--------------------- 
+SABOR EXPRESS
+--------------------- 
     """)
 def exibir_opcoes():
     print("1. Cadastrar Restaurante")
     print("2. Listar Restaurante")
     print("3. Ativar Restaurante")
     print("4. Sair\n")
+
 def finalizar_app():
     os.system("cls") # Limpando o CMD
     print("Finalizando programa...")
-def escolher_opcao():
-    opcao_escolhida = int(input("Escolha uma opção: "))
 
-    match opcao_escolhida:
-        case 1:
-            print("Cadastrar Restaurante")
-        case 2:
-            print("Listar  Restaurante")
-        case 3:
-            print("Ativar Restaurante")
-        case 4:
-            finalizar_app()
-        case _:
-            print("Opção inválida!")
+def opcao_invalida():
+    print("Opção inválida\n")
+    input("Digite qualquer tecla para voltar ao menu principal")
+    main()
+
+def escolher_opcao():
+    try:
+        opcao_escolhida = int(input("Escolha uma opção: "))
+
+        match opcao_escolhida:
+            case 1:
+                print("Cadastrar Restaurante")
+            case 2:
+                print("Listar  Restaurante")
+            case 3:
+                print("Ativar Restaurante")
+            case 4:
+                finalizar_app()
+            case _:
+                opcao_invalida()
+    except:
+        opcao_invalida()
 
 # Definindo este arquivo como o principal do programa
 def main():
+    os.system("cls")
     exibir_nome_programa()
     exibir_opcoes()
     escolher_opcao()
