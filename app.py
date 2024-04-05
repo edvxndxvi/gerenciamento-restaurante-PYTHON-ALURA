8459d638e4d4db73da1145a113728f03b7002927
edvxndxvi <edvandavi1@hotmail.com>
2024-04-05 12:47:37 -0300

    Criando cadastro de restaurantes

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import os #Importando biblioteca Python
 
+restaurantes = []
+
 def exibir_nome_programa():
     print("""
 --------------------- 
 SABOR EXPRESS
 --------------------- 
     """)
+
 def exibir_opcoes():
     print("1. Cadastrar Restaurante")
     print("2. Listar Restaurante")
@@ -21,13 +24,23 @@ def opcao_invalida():
     input("Digite qualquer tecla para voltar ao menu principal")
     main()
 
+def cadastrar_restaurante():
+    os.system('cls')
+    exibir_nome_programa()
+    print("Cadastro de restaurantes\n")
+    nome_restaurante = input("Digite o nome do restaurante que deseja cadastrar: ")
+    restaurantes.append(nome_restaurante)
+    print(f"O resutarante {nome_restaurante} foi cadastrado com sucesso!\n")
+    input("Digite qualquer tecla para voltar ao menu principal")
+    main()
+
 def escolher_opcao():
     try:
         opcao_escolhida = int(input("Escolha uma opção: "))
 
         match opcao_escolhida:
             case 1:
-                print("Cadastrar Restaurante")
+                cadastrar_restaurante()
             case 2:
                 print("Listar  Restaurante")
             case 3:
